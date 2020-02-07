86a79dfcb2f8c11e4ddfc94fd7762a542f012764
Juan Diego Barrios <jd.barriosc@uniandes.edu.co>
2020-02-06 22:29:30 -0500

    Updates

---
+++ precache-manifest.d990f5bd0be75f5bfdd681c7ae4cba49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989cfef5f6e09fa74317c6db20ff07a3",
+    "url": "/personal-page-front/./index.html"
+  },
+  {
+    "revision": "66c3ffa209a9e2662a4a",
+    "url": "/personal-page-front/./static/css/main.1700c942.chunk.css"
+  },
+  {
+    "revision": "365b8431022ac0578cb2",
+    "url": "/personal-page-front/./static/js/2.6854c6d9.chunk.js"
+  },
+  {
+    "revision": "bc60939987866ab6ad76618ca79a0fec",
+    "url": "/personal-page-front/./static/js/2.6854c6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c3ffa209a9e2662a4a",
+    "url": "/personal-page-front/./static/js/main.d24cdcca.chunk.js"
+  },
+  {
+    "revision": "8af47e1e95d43f8ac292",
+    "url": "/personal-page-front/./static/js/runtime-main.1e2ee99a.js"
+  }
+]);
